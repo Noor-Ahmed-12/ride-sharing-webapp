51da7974a1ac3e1639556f43e8fbeb9753d6dc6e
Noor Ahmed Shaikh <60597502+Noor-Ahmed-12@users.noreply.github.com>
2023-09-30 03:35:48 -0700

    Update carpooling.js

--- carpooling.js
+++ carpooling.js
@@ -1,10 +1,10 @@
 // carpooling.js
 
-// Simulated ride data
+// Simulated ride data with prices
 const rides = [
-    { id: 1, driver: 'Alice', origin: 'City A', destination: 'City B', seatsAvailable: 3 },
-    { id: 2, driver: 'Bob', origin: 'City C', destination: 'City D', seatsAvailable: 1 },
-    { id: 3, driver: 'Carol', origin: 'City X', destination: 'City Y', seatsAvailable: 5 },
+    { id: 1, driver: 'Alice', origin: 'City A', destination: 'City B', seatsAvailable: 3, price: 20 },
+    { id: 2, driver: 'Bob', origin: 'City C', destination: 'City D', seatsAvailable: 1, price: 15 },
+    { id: 3, driver: 'Carol', origin: 'City X', destination: 'City Y', seatsAvailable: 5, price: 30 },
     // Add more ride objects as needed
 ];
 
@@ -12,24 +12,32 @@ const rideList = document.getElementById('ride-list');
 const bookButton = document.getElementById('book-button');
 const bookingForm = document.getElementById('booking-form');
 const selectedRideSelect = document.getElementById('selected-ride');
+const priceRangeInput = document.getElementById('price-range');
+const priceOutput = document.getElementById('price-output');
+const filterForm = document.getElementById('filter-form');
 
 // Function to display rides in the UI
 function displayRides() {
     rideList.innerHTML = '';
-    rides.forEach((ride) => {
-        const li = document.createElement('li');
-        li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
-        li.innerHTML = `
-            Ride ${ride.id} - ${ride.origin} to ${ride.destination}
-            <span class="badge bg-primary rounded-pill">${ride.seatsAvailable} seats available</span>
-        `;
-        rideList.appendChild(li);
+    const priceRange = priceRangeInput.value;
 
-        // Populate the booking form's ride options dynamically
-        const option = document.createElement('option');
-        option.value = ride.id;
-        option.textContent = `Ride ${ride.id} - ${ride.origin} to ${ride.destination}`;
-        selectedRideSelect.appendChild(option);
+    rides.forEach((ride) => {
+        if (ride.price <= priceRange) {
+            const li = document.createElement('li');
+            li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
+            li.innerHTML = `
+                Ride ${ride.id} - ${ride.origin} to ${ride.destination} 
+                <span class="badge bg-primary rounded-pill">Price: $${ride.price}</span>
+                <span class="badge bg-success rounded-pill">${ride.seatsAvailable} seats available</span>
+            `;
+            rideList.appendChild(li);
+
+            // Populate the booking form's ride options dynamically
+            const option = document.createElement('option');
+            option.value = ride.id;
+            option.textContent = `Ride ${ride.id} - $${ride.price} (${ride.origin} to ${ride.destination})`;
+            selectedRideSelect.appendChild(option);
+        }
     });
 }
 
@@ -65,4 +73,15 @@ bookingForm.addEventListener('submit', (e) => {
     } else {
         alert('Booking failed. No seats available for the selected ride.');
     }
+});
+
+// Event listener for filtering rides based on price range
+filterForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    displayRides();
+});
+
+// Event listener to update the displayed price range value
+priceRangeInput.addEventListener('input', () => {
+    priceOutput.textContent = `$${priceRangeInput.value}`;
 });
